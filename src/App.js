62f939ee1ab247ffbc3cc67c2e8ae52c41070fd7
unknown <sjha23234@gmail.com>
2023-01-16 16:47:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Seatbook1 from './Seatbook1';
+import Seatbook2 from './Seatbook2';
+
+function App() {
+  return (
+    <div>
+      <Seatbook1/>
+      <Seatbook2/>
+
+
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Seatbook1 from "./Seatbook1";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <Seatbook1 />
+  </React.StrictMode>,
+  rootElement
+);
